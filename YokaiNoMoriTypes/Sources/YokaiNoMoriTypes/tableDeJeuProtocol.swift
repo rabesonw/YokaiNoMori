a8cce0b3a122d5bd3233e3aa3d610cbefc1ccaec
Laurancy-Dorian <laurancy.dorian@gmail.com>
2018-12-19 10:31:30 +0100

    FEATURE : Corps du fichier tableDeJeu

--- YokaiNoMoriTypes/Sources/YokaiNoMoriTypes/tableDeJeuProtocol.swift
+++ YokaiNoMoriTypes/Sources/YokaiNoMoriTypes/tableDeJeuProtocol.swift
@@ -5,15 +5,15 @@ public protocol tableDeJeuProtocol : Sequence {
     typealias Piece = pieceProtocol
     typealias Joueur = joueurProtocol
     typealias CollectionPositions = collectionPositionsProtocol
-	
+
 	// l'iterateur associe
 	associatedtype tableDeJeuIterateurProtocol : IteratorProtocol
 	associatedtype Reserve : reserveProtocol
-    
-    
+
+
 	var joueur1 : Joueur{get set}
 	var joueur2 : Joueur{get set}
-    
+
     var reserve1 : Reserve{get}
     var reserve2 : Reserve{get}
 
@@ -36,30 +36,30 @@ public protocol tableDeJeuProtocol : Sequence {
 
 	// validerDeplacement : tableDeJeu x Piece x Int x Int -> Bool
 	// verifie qu'une Piece a bien le droit d'aller à l'emplacement indique
-	// Pre : aucune 
-	// Post : renvoie True si le deplacement respecte les regles du jeu : 
+	// Pre : aucune
+	// Post : renvoie True si le deplacement respecte les regles du jeu :
 	//		la position (neufX, neufY) est atteignable de la position courante de la piece donnee en premier parametre
 	//		la case (neufX, neufY) est vide
 	//		1 <= x <= 3 et 1 <= y <=4.
 	//		renvoie False sinon.
 	func validerDeplacement(_ Piece : Piece, _ neufX : Int, _ neufY : Int) -> Bool
 
-	
+
     // validerCapture : tableDeJeu x Piece x Int x Int -> Bool
 	// verifie qu'une Piece a bien le droit d'attaquer à l'emplacement indique
 	// Pre: aucune
-	// Post : renvoie True si le deplacement respecte les regles du jeu : 
+	// Post : renvoie True si le deplacement respecte les regles du jeu :
 	//		la position (neufX, neufY) est atteignable de la position courante de la piece donnee en premier parametre
 	//		la case (neufX, neufY) est occupee par une piece ennemie
 	//		1 <= x <= 3 et 1 <= y <=4.
 	//		renvoie False sinon.
 	func validerCapture(_ Piece : Piece, _ neufX : Int, _ neufY : Int) -> Bool
-    
-    
+
+
 	// deplacerPiece : tableDeJeu x Piece x Int x Int -> tableDeJeu
-	// deplace une Piece d'une position à une autre 
+	// deplace une Piece d'une position à une autre
 	// Pre : le deplacement est valide, conforme au validerDeplacement
-	// Post : si les preconditions sont satisfaites, la position de depart est vide, 
+	// Post : si les preconditions sont satisfaites, la position de depart est vide,
 	//		la Piece est à la position voulue. Sinon, l’etat de la table de jeu reste le meme.
 	//		Apres on deplace la piece, on verifie si on doit la promouvoir, en appelant
 	//		estEnPromotion(piece) pour verifier, et transformerKodama(piece) pour la transformer.
@@ -69,7 +69,7 @@ public protocol tableDeJeuProtocol : Sequence {
 	// capturerPiece : tableDeJeu x Piece x Piece -> tableDeJeu
 	// capture une pièce de l’autre joueur (donnee par le deuxieme parametre) avec une Piece de le joueur courant
 	// Pre : la capture est valide, conforme au validerDeplacement
-	// Post : si les preconditions sont satisfaites, les deux Pieces changent leurs positions 
+	// Post : si les preconditions sont satisfaites, les deux Pieces changent leurs positions
 	//	et la pièce capturee est dans la reserve de le joueur attaquant . Sinon, l’etat de la table de jeu reste le meme.
 	@discardableResult
 	mutating func capturerPiece(_ pieceAttaquante : Piece, _ neufX : Int, _ neufY : Int) -> Self
@@ -78,7 +78,7 @@ public protocol tableDeJeuProtocol : Sequence {
 	// transforme un "kodama" en "kodama samourai" ou la chose inverse.
 	// Fontion appelle a la fin de capturerPiece(): on verifie si on a capture un kodama samurai
 	// et a la fin de deplacerPiece(), on verifie si on a deplace la piece dans la zone de promotion
-	// Pre : la Piece est un kodama et se trouve dans la zone de promotion 
+	// Pre : la Piece est un kodama et se trouve dans la zone de promotion
 	//        ou c'est un kodama samurai qui est capture
 	// Post : la nouvelle Piece est au même emplacement mais est un kodama samourai
 	//        ou c'est un kodama dans la reserve de l'attaquant
@@ -86,7 +86,7 @@ public protocol tableDeJeuProtocol : Sequence {
 	mutating func transformerKodama(_ piece : Piece) throws -> Self
 
     // mettreEnReserve : tableDeJeu x Piece -> tableDeJeu
-    // met un pion en reserve en changeant son joueur d'origine 
+    // met un pion en reserve en changeant son joueur d'origine
     // Pre : la pièce est sur la table de jeu (contre-exemple : etre deja en reserve)
     // Post : la Piece est en reserve et son joueur est changé
 	@discardableResult
@@ -103,7 +103,7 @@ public protocol tableDeJeuProtocol : Sequence {
 
 	// gagnerPartie : tableDeJeu x Joueur -> Bool
 	// verifie si la partie est gagnée par le joueur indique par le parametre
-	// Pre : aucune 
+	// Pre : aucune
 	// Post : renvoie true si le jouer donne a gagne, false sinon
 	func gagnerPartie(_ joueur : Joueur) -> Bool
 
@@ -113,7 +113,7 @@ public protocol tableDeJeuProtocol : Sequence {
 
 }
 
-protocol tableDeJeuIterateurProtocol : IteratorProtocol {
+public protocol tableDeJeuIterateurProtocol : IteratorProtocol {
 
     typealias Piece = pieceProtocol
 
@@ -122,6 +122,6 @@ protocol tableDeJeuIterateurProtocol : IteratorProtocol {
     // Pre : aucune
     // Post : retourne la piece suivante dans la collection du tableDeJeu, ou nil si on est au fin de la collection
 
-	func next() -> Piece? 
+	func next() -> Piece?
 
 }
