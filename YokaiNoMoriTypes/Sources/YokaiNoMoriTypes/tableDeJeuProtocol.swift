7d41ac4b6d8ea3662c93be95dd06a0d2d9d78724
Laurancy-Dorian <laurancy.dorian@gmail.com>
2019-01-09 09:00:20 +0100

    Edit TDJ Protocol

--- YokaiNoMoriTypes/Sources/YokaiNoMoriTypes/tableDeJeuProtocol.swift
+++ YokaiNoMoriTypes/Sources/YokaiNoMoriTypes/tableDeJeuProtocol.swift
@@ -6,9 +6,12 @@ public protocol tableDeJeuProtocol : Sequence {
     typealias Joueur = joueurProtocol
     typealias CollectionPositions = collectionPositionsProtocol
 
+    // EDIT
+    typealias Reserve = reserveProtocol
+
 	// l'iterateur associe
 	associatedtype tableDeJeuIterateurProtocol : IteratorProtocol
-	associatedtype Reserve : reserveProtocol
+	 // associatedtype Reserve : reserveProtocol
 
 
 	var joueur1 : Joueur{get set}
