399d061a2dc20c08146433fc7751894b3533bcf4
Laurancy-Dorian <laurancy.dorian@gmail.com>
2019-01-12 17:30:04 +0100

    Installation des dependences dans Main et Test avec Types

--- YokaiNoMoriMain/Package.swift
+++ YokaiNoMoriMain/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "YokaiNoMoriMain",
-            dependencies: []),
+            dependencies: ["YokaiNoMoriTypes", "YokaiNoMoriStruct"]),
         .testTarget(
             name: "YokaiNoMoriMainTests",
             dependencies: ["YokaiNoMoriMain"]),
